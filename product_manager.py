b9b1416e3dccdbf9af2dc9b2dd6a796ad564e604
lqnga112 <lqnga112@gmail.com>
2026-02-23 12:22:42 +0700

    Xay dung ham load du lieu tu file

--- product_manager.py
+++ product_manager.py
@@ -5,3 +5,14 @@ import json
 import os
 
 DATA_FILE = "products.json"
+def load_data():
+    if not os.path.exists(DATA_FILE):
+        return []
+
+    try:
+        f = open(DATA_FILE, "r", encoding="utf-8")
+        data = json.load(f)
+        f.close()
+        return data
+    except:
+        return []
